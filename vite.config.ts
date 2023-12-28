a5a7d51af97548a1d70e7aec7d43041ae64c31c2
Mark Domahidi <mdomahidi@gmail.com>
2023-12-28 15:17:32 +0100

    feat: introduce svgr plugin to load svgs as components

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,17 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@components": path.resolve(__dirname, "./src/components"),
       "@hooks": path.resolve(__dirname, "./src/hooks"),
       "@styles": path.resolve(__dirname, "./src/styles"),
       "@utils": path.resolve(__dirname, "./src/utils"),
+      "@svgs": path.resolve(__dirname, "./public/svgs"),
     },
   },
   test: {
