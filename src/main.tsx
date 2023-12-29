6c9efb92f4bcecac0dd6f7625f4dcea1d3169d31
Mark Domahidi <mdomahidi@gmail.com>
2023-12-29 18:25:22 +0100

    feat: introduce react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,29 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
+import { envConfig } from "@configs/env.config";
 import { GlobalStyle } from "@styles/global";
 import { theme } from "@styles/theme";
 import { App } from "./app";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+      {envConfig.mode === "development" && <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />}
+    </QueryClientProvider>
   </React.StrictMode>
 );
