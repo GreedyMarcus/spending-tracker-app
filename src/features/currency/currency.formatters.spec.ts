92f2d2ae09361856486142d219bccfd896e6de44
Mark Domahidi <mdomahidi@gmail.com>
2023-12-29 17:50:52 +0100

    feat: add thousand separator in currency formatter

--- src/features/currency/currency.formatters.spec.ts
+++ src/features/currency/currency.formatters.spec.ts
@@ -4,10 +4,18 @@ import { Currency } from "./currency.types";
 describe("Currency Formatters", () => {
   describe("CurrencyFormatter", () => {
     it.each<[number, Currency, string]>([
-      [1000, Currency.HUF, "1000Ft"],
-      [1000.5, Currency.HUF, "1001Ft"],
+      [100, Currency.HUF, "100Ft"],
+      [1000, Currency.HUF, "1,000Ft"],
+      [1000.5, Currency.HUF, "1,001Ft"],
+      [10000, Currency.HUF, "10,000Ft"],
+      [100000, Currency.HUF, "100,000Ft"],
+      [1000000, Currency.HUF, "1,000,000Ft"],
       [10, Currency.USD, "$10.00"],
       [10.5, Currency.USD, "$10.50"],
+      [1000, Currency.USD, "$1,000.00"],
+      [10000, Currency.USD, "$10,000.00"],
+      [100000, Currency.USD, "$100,000.00"],
+      [1000000, Currency.USD, "$1,000,000.00"],
     ])("should format '%s %s' as '%s'", async (amount, currency, expectedFormat) => {
       expect(CurrencyFormatter.format(amount, currency)).toEqual(expectedFormat);
     });
--- src/features/currency/currency.formatters.ts
+++ src/features/currency/currency.formatters.ts
@@ -4,9 +4,13 @@ export const CurrencyFormatter = {
   format(amount: number, currency: Currency): string {
     switch (currency) {
       case Currency.HUF:
-        return `${amount.toFixed(0)}Ft`;
+        return `${amount.toLocaleString("en-US", { maximumFractionDigits: 0 })}Ft`;
       case Currency.USD:
-        return `$${amount.toFixed(2)}`;
+        return amount.toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+          maximumFractionDigits: 2,
+        });
     }
   },
 } as const;
